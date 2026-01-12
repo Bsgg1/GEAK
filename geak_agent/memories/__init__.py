a86a644b85bcd027204d52615147a60fda71024e
balatoth <balatoth@amd.com>
2026-01-12 15:15:19 +0100

    Convert GEAK-Agent to installable pip package

---
+++ geak_agent/memories/__init__.py
@@ -0,0 +1,10 @@
+# Copyright(C) [2025] Advanced Micro Devices, Inc. All rights reserved.
+
+from geak_agent.memories.Memory import MemoryClassMeta, BaseMemory, ReflexionMemory
+
+__all__ = [
+    "MemoryClassMeta",
+    "BaseMemory",
+    "ReflexionMemory",
+]
+
--- src/args_config.py
+++
@@ -1,9 +0,0 @@
-# Copyright(C) [2025] Advanced Micro Devices, Inc. All rights reserved.
-
-import yaml
-from argparse import Namespace
-
-def load_config(yaml_path):
-    with open(yaml_path, 'r') as f:
-        config_dict = yaml.safe_load(f)
-    return Namespace(**config_dict)
