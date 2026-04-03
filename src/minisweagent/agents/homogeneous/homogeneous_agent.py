2264649af740b04aea1ad207db9b5b96855a393a
yueliu14 <yue.liu4@amd.com>
2026-04-03 03:18:06 +0000

    concate repo path to task content, fix profile_kernel disable bug

--- src/minisweagent/agents/homogeneous/homogeneous_agent.py
+++ src/minisweagent/agents/homogeneous/homogeneous_agent.py
@@ -117,6 +117,7 @@ def run_homogeneous_agent(
     agent = ParallelAgent(model, env, **agent_config)
 
     try:
+        task_content = task_content + "\n\n" + "The current worktree is: " + str(final_repo)
         best_result = agent.run(
             task_content,
             output=final_traj_output,
